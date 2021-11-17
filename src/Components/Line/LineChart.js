8ed0c11b7d3bdfc770e155adf50cb2f22699cca9
Boris Grunwald <borisgrunwald@gmail.com>
2021-11-17 16:44:12 +0100

    Cleanup

--- src/Components/Line/LineChart.js
+++ src/Components/Line/LineChart.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Line } from '@ant-design/charts';
-import data from '../../Mockdata/c02emission.json'
+import React from 'react';
+import { Line } from '@ant-design/charts'; 
 
 
 /**
