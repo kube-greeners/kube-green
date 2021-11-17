4292a669116ade42fecd36081b13c2102c8d2d46
Boris Grunwald <borisgrunwald@gmail.com>
2021-11-17 12:51:24 +0100

    Basic linechart done

---
+++ src/Components/Line/LineChart.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Line } from '@ant-design/charts';
+import data from '../../Mockdata/c02emission.json'
+
+
+/**
+ * @param {Array} data - The data to show
+ * @param {string} xField - Name of the x axis values
+ * @param {string} yField - Name of the y axis values
+ */
+const LineChart = ({data,xField,yField}) => {
+
+
+   
+    const config = {
+        data: data,
+        padding: 'auto',
+        padding:100,
+        xField,
+        yField,
+        xAxis: { tickCount: 10 },
+        slider: {
+            start: 0.9,
+            end: 1,
+        },
+    };
+    return <Line {...config} />;
+};
+
+export default LineChart;
+
