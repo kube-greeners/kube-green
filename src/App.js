e8a5466769bb28ba1146d7477f04957318a9e682
Boris Grunwald <borisgrunwald@gmail.com>
2021-11-19 16:42:03 +0100

    Added cart titles

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
     <div className="container">
       <Row gutter={[24, 24]}>
         <Col span={16}>
-          <Card title="Line chart">lorem</Card>
+          <Card title="Line chart">test</Card>
         </Col>
         <Col span={8}>
           <Row gutter={[0,24]}>
-            <Col flex="0 0 100%"><Card title="Line chart">lorem</Card></Col>
-            <Col flex="0 0 100%"><Card title="Line chart">lorem</Card></Col>
-            <Col flex="0 0 100%"><Card title="Line chart">lorem</Card></Col>
-            <Col flex="0 0 100%"><Card title="Line chart">lorem</Card></Col>
+            <Col flex="0 0 100%"><Card title="Saved Emission">test</Card></Col>
+            <Col flex="0 0 100%"><Card title="CPU Usage and Allocation">test</Card></Col>
+            <Col flex="0 0 100%"><Card title="Memory Usage  and Allocation">test</Card></Col>
+            <Col flex="0 0 100%"><Card title="N Active Pod">test</Card></Col>
           </Row>
         </Col>
       </Row>
