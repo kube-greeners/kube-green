b13240631f79e7cccf2e0be65986ccc909ba2f02
Boris Grunwald <borisgrunwald@gmail.com>
2021-11-25 17:06:34 +0100

    Fetching pod data and storing in redux

--- src/Utilities/dataFetching.js
+++ src/Utilities/dataFetching.js
@@ -5,3 +5,10 @@ export const fetchCO2EmissionData = createAsyncThunk('dashboard/fetchco2emission
      return await fetch(`${process.env.REACT_APP_API_BASE_URL}/cpu?namespace=${namespace}&interval=${interval}}&step=${step}`)
      .then(res => res.json()) 
   })
+
+
+export const fetchActivePods = createAsyncThunk('dashboard/fetchpods', async ({namespace,interval,step}) => {
+    //This url will fetch the CPU usage for each pod. It should be changed when KG-121 is done to the correct URL 
+     return await fetch(`${process.env.REACT_APP_API_BASE_URL}/pods?namespace=${namespace}&interval=${interval}}&step=${step}`)
+     .then(res => res.json()) 
+})
