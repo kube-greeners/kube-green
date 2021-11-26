9da8b69e002797a83fd1490e6cd6060248e52694
Boris Grunwald <borisgrunwald@gmail.com>
2021-11-26 20:47:34 +0100

    Removed comment

--- src/Utilities/dataFetching.js
+++ src/Utilities/dataFetching.js
@@ -8,7 +8,6 @@ export const fetchCO2EmissionData = createAsyncThunk('dashboard/fetchco2emission
 
 
 export const fetchActivePods = createAsyncThunk('dashboard/fetchpods', async ({namespace,interval,step}) => {
-    //This url will fetch the CPU usage for each pod. It should be changed when KG-121 is done to the correct URL 
      return await fetch(`${process.env.REACT_APP_API_BASE_URL}/pods?namespace=${namespace}&interval=${interval}}&step=${step}`)
      .then(res => res.json()) 
 })
