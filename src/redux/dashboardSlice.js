12326da835cd2888c4941c1ca0662b86db667660
Ragnhild <ragnhildk98@gmail.com>
2021-11-29 14:06:28 +0100

    save memory allocation, memory usage, cpu allocation data in redux

--- src/redux/dashboardSlice.js
+++ src/redux/dashboardSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { fetchCO2EmissionData,fetchActivePods,fetchCPU } from '../Utilities/dataFetching'
+import { fetchCO2EmissionData,fetchActivePods, fetchCpuUsage, fetchCpuAllocation, fetchMemoryUsage, fetchMemoryAllocation } from '../Utilities/dataFetching'
 
 
 const initialState = {
@@ -8,15 +8,20 @@ const initialState = {
     data: [],
   },
   cpu: {
-    status:'idle',
+    statusUsage:'idle',
+    statusAllocation:'idle',
     allocated:[],
     usage:[],
     currentAllocated:0,
     currentUsage:0
   },
   memory:{
-    status:'idle',
-    data: [],
+    statusUsage:'idle',
+    statusAllocation:'idle',
+    allocated:[],
+    usage:[],
+    currentAllocated:0,
+    currentUsage:0
   },
   pods: {
     status:'idle',
@@ -49,22 +54,57 @@ export const dashboardSlice = createSlice({
         state.pods.status = 'succeeded';
         state.pods.currentValue = action.payload[0].values.pop().pop();
         state.pods.data = action.payload[0].values;
-        
       })
       .addCase(fetchActivePods.rejected, (state, action) => {
         state.pods.status = 'failed'
       })
-      .addCase(fetchCPU.pending, (state, action) => {
-        state.cpu.status = 'loading'
+      .addCase(fetchCpuUsage.pending, (state, action) => {
+        state.cpu.statusUsage = 'loading'
       })
-      .addCase(fetchCPU.fulfilled, (state, action) => {
-        state.cpu.status = 'succeeded';
+      .addCase(fetchCpuUsage.fulfilled, (state, action) => {
+        state.cpu.statusUsage = 'succeeded';
         state.cpu.currentUsage = action.payload[0].values.pop().pop();
         state.cpu.usage = action.payload[0].values;
-        
       })
-      .addCase(fetchCPU.rejected, (state, action) => {
-        state.cpu.status = 'failed'
+      .addCase(fetchCpuUsage.rejected, (state, action) => {
+        state.cpu.statusUsage = 'failed'
+      })
+      .addCase(fetchCpuAllocation.pending, (state, action) => {
+        state.cpu.statusAllocation = 'loading'
+      })
+      .addCase(fetchCpuAllocation.fulfilled, (state, action) => {
+        state.cpu.statusAllocation = 'succeeded';
+        if(action.payload.values.length !== 0){
+          state.cpu.currentAllocated = action.payload[0].values.pop().pop();
+          state.cpu.allocated = action.payload[0].values;
+        }
+      })
+      .addCase(fetchCpuAllocation.rejected, (state, action) => {
+        state.cpu.statusAllocation = 'failed'
+      })
+      .addCase(fetchMemoryUsage.pending, (state, action) => {
+        state.memory.statusUsage = 'loading'
+      })
+      .addCase(fetchMemoryUsage.fulfilled, (state, action) => {
+        state.memory.statusUsage = 'succeeded';
+        state.memory.currentUsage = action.payload[0].values.pop().pop();
+        state.memory.usage = action.payload[0].values;
+      })
+      .addCase(fetchMemoryUsage.rejected, (state, action) => {
+        state.memory.statusUsage = 'failed'
+      })
+      .addCase(fetchMemoryAllocation.pending, (state, action) => {
+        state.cpu.statusAllocation = 'loading'
+      })
+      .addCase(fetchMemoryAllocation.fulfilled, (state, action) => {
+        state.memory.statusAllocation = 'succeeded';
+        if(action.payload[0].values.length !== 0){
+          state.memory.currentAllocated = action.payload[0].values.pop().pop();
+          state.memory.allocated = action.payload[0].values;
+        }
+      })
+      .addCase(fetchMemoryAllocation.rejected, (state, action) => {
+        state.memory.statusAllocation = 'failed'
       })
   }
 })
