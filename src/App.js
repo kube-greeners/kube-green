85a83865f009e3a70f5b0ae4f2753dde915d2c9c
Boris Grunwald <borisgrunwald@gmail.com>
2021-11-29 13:48:57 +0100

    Changed line chart title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="container">
       <Row gutter={[24, 24]}>
         <Col span={16}>
-          <Card  style={{height:'100%', position:'relative'}} title="Line chart"><Co2Emission/> </Card>
+          <Card  style={{height:'100%', position:'relative'}} title="Estimated Co2 emission"><Co2Emission/> </Card>
         </Col>
         <Col span={8} className="flexcolumn">
             <Card style={{flex:'1'}} title="Saved Emission">test</Card>
