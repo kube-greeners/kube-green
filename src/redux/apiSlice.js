b4e50ce9f03d7cf75638c13ba2c48e38b993948f
Boris Grunwald <borisgrunwald@gmail.com>
2021-12-08 09:23:45 +0100

    Added get co2 emission query

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -5,6 +5,9 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_BASE_URL }),
   endpoints: (builder) => ({
+    getCO2Emission: builder.query({
+      query: ({namespace,interval,step}) => `/co2_emission_with_kube_green?namespace=${namespace}&interval=${interval}&step=${step}`
+    }),
   }),
 })
 
