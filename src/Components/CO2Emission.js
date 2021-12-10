4e58491857198c0f64beb6bd5c9ecbea8b868624
Hanna <hanntorj@stud.ntnu.no>
2021-12-10 14:02:32 +0100

    removed unused variables in CO2emission component

--- src/Components/CO2Emission.js
+++ src/Components/CO2Emission.js
@@ -2,18 +2,17 @@ import LoadingSpinner from "./LoadingSpinner/LoadingSpinner";
 import LineChart from "./Line/LineChart";
 import React from 'react';
 import { useGetCO2EmissionQuery } from "../redux/apiSlice";
-import { convertDate } from "../Utilities/utilityFunctions";
 
 
 
 function Co2Emission() {
   const {
     data: usage,
-    isLoading,
+    // isLoading,
     isFetching,
     isSuccess,
-    isError,
-    error
+    // isError,
+    // error
   } = useGetCO2EmissionQuery({namespace:"production",interval:"5d",step:"1h"});
 
   
