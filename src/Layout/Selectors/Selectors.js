a28f2442fc4340ded406c1d6fc5e9ebf486c48a9
Ragnhild <ragnhildk98@gmail.com>
2021-12-10 11:44:40 +0100

    Add date picker

--- src/Layout/Selectors/Selectors.js
+++ src/Layout/Selectors/Selectors.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Col, Row, Select } from 'antd'
+import { Col, Row, Select, DatePicker } from 'antd'
 import { useSelector } from 'react-redux'
+import moment from 'moment';
 const { Option } = Select;
 
 export default function Selectors() {
 
+    const {RangePicker} = DatePicker;
+
+    const dateFormat = 'YYYY/MM/DD';
+
     const {namespaces,resources} = useSelector(state => state.dashboard.selects);
+    //const {startDate, endDate} = useSelector(state => state.dashboard.interval);
     
     const nameSpaceSelected = ns => {
         
@@ -13,6 +19,16 @@ export default function Selectors() {
 
     const resourceSelected = rs => {
         
+    }
+
+    const intervalSelected = rs => {
+        
+    }
+
+    const labelStyle = {
+        marginBottom:'.5rem',
+        display:'block',
+        color:'#666666'
     }
 
     return (
@@ -23,6 +39,7 @@ export default function Selectors() {
                     data={namespaces.data} 
                     name="Namespace" 
                     defaultVal={namespaces.currentlySelected} 
+                    labelStyle={labelStyle}
                     style={{ display: 'block' }} 
                     onChange={nameSpaceSelected} />
                 </Col>
@@ -31,21 +48,24 @@ export default function Selectors() {
                         data={resources.data}
                         name="Resource" 
                         defaultVal={resources.currentlySelected} 
+                        labelStyle={labelStyle}
                         style={{ display: 'block' }} 
                         onChange={resourceSelected} />
+                </Col>
+                <Col span={7}>
+                    <label style={labelStyle}>Time interval</label>
+                    <RangePicker
+                        defaultValue={[moment('2021/12/01', dateFormat), moment('2021/12/09', dateFormat)]}
+                        format={dateFormat}
+                        disabled={[false, true]} //could be removed
+                        onChange={intervalSelected}/>    
                 </Col>
             </Row>
         </>
     )
 }
 
-function Selector({ data, name,defaultVal , ...rest }) {
-
-    const labelStyle = {
-        marginBottom:'.5rem',
-        display:'block',
-        color:'#666666'
-    }
+function Selector({ data, name, defaultVal, labelStyle, ...rest }) {
 
     return (
         <>
