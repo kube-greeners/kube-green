d9f9f7937de4a5a27d51e546e37a5d40076c2115
Hanna <hanntorj@stud.ntnu.no>
2021-12-10 13:52:22 +0100

    snapshot of app

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import App from "./App";
 import renderer from 'react-test-renderer'
 import store from "./redux/store";
-import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 
 test("matches snapshot", () => {
