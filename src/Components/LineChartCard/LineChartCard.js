5b4c03015acbcb7d8b8bc7a7f8e4cac289aabf17
Boris Grunwald <borisgrunwald@gmail.com>
2021-12-12 15:31:04 +0100

    Finished lineChartCard

--- src/Components/LineChartCard/LineChartCard.js
+++ src/Components/LineChartCard/LineChartCard.js
@@ -1,9 +1,20 @@
+import { Card } from 'antd';
 import React from 'react';
+import LineChart from '../LineChart/LineChart';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 
 
-function LineChartCard({dataFetching,currentlyShowing}) {
-    return <></>
+function LineChartCard({ dataFetching, currentlyShowing }) {
+    return (
+        <Card style={{ gridArea: 'lc' }} title={currentlyShowing}>
+            {!dataFetching.isFetching && dataFetching.isSuccess ?
+                <LineChart data={dataFetching.data.historicalData} />
+                : <div style={{ height: 500, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <LoadingSpinner/>
+                </div>
+            }
+        </Card>)
 }
 
 export default LineChartCard
