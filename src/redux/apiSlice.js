9f240ba281b151e56a2fdabf7eb19ce77c372b41
Boris Grunwald <borisgrunwald@gmail.com>
2021-12-16 14:56:27 +0100

    use createQueryParams funciton in queries

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -38,33 +38,33 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_BASE_URL }),
   endpoints: (builder) => ({
     getCO2Emission: builder.query({
-      query: ({namespace, startDate, endDate}) => `/co2_emission_with_kube_green?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({namespace, startDate, endDate}) => `/co2_emission_with_kube_green${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse: resData => {
         return {historicalData:convertHistoricalData(resData[0].values,"Grams of CO2")}
       }
     }),
     getPods: builder.query({
-      query: ({namespace, startDate, endDate}) => `/all_active_pods?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({namespace, startDate, endDate}) => `/all_active_pods${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse : resData => convertData(resData, "Number of pods")
     }),
     getCpuUsage: builder.query({
-      query: ({namespace, startDate, endDate}) => `/cpu_usage?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({namespace, startDate, endDate}) => `/cpu_usage${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse : resData => convertData(resData, "CPU Usage")
     }),
     getCpuAllocation: builder.query({
-      query: ({namespace, startDate, endDate}) => `cpu_allocation?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({namespace, startDate, endDate}) => `cpu_allocation${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse : resData => convertData(resData, "CPU Allocation")
     }),
     getMemoryUsage: builder.query({
-      query: ({namespace, startDate, endDate}) => `/memory_usage?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({namespace, startDate, endDate}) => `/memory_usage${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse : resData => convertData(resData, "Memory Usage")
     }),
     getMemoryAllocation: builder.query({
-      query: ({namespace, startDate, endDate}) => `/memory_allocation?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({namespace, startDate, endDate}) => `/memory_allocation${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse : resData => convertData(resData, "Memory Allocation")
     }),
     getSavedEmission: builder.query({
-      query: ({startDate, endDate,namespace}) => `saved_co2_emission?namespace=${namespace}&start=${startDate}&end=${endDate}`,
+      query: ({startDate, endDate,namespace}) => `saved_co2_emission${createQueryparams(namespace,startDate,endDate)}`,
       transformResponse: resData => {
         return parseFloat(resData[0].values.pop().pop())
       }
